efb82ab16a3c06a623a7f1b14184164ffcb8edf2
charles.silva <charles.silva@grupoicts.com.br>
2024-05-09 20:11:44 -0400

    Validar Contrato de editar usuario

--- src/test/java/com/api_java_test/contrato/UpdateUserTest.java
+++ src/test/java/com/api_java_test/contrato/UpdateUserTest.java
@@ -2,16 +2,14 @@ package com.api_java_test.contrato;
 
 import com.api_java_test.baseteste.BaseTest;
 import com.api_java_test.dataprovider.EditarProvider;
-import com.api_java_test.dataprovider.UsuarioProvider;
 import com.api_java_test.dto.EditarDTO;
-import com.api_java_test.dto.UsuarioDTO;
 import io.restassured.http.ContentType;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
-public class EditUserTest extends BaseTest {
+public class UpdateUserTest extends BaseTest {
 
     @Test(dataProvider = "editarUsuario", dataProviderClass = EditarProvider.class)
     public void validarContratoEditarUsuario(EditarDTO editar) {
